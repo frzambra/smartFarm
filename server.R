27be1afa9f12848b025477ab5ebc09e215e1e381
frzambra <frzambra@gmail.com>
2017-10-10 11:01:04 -0300

    first commit

---
+++ server.R
@@ -0,0 +1,118 @@
+library(shiny)
+library(tidyverse)
+library(shinydashboard)
+library(leaflet)
+library(leaflet.extras)
+library(RColorBrewer)
+library(highcharter)
+
+function(input, output, session) {
+  
+  output$mapSelect <- renderLeaflet({
+    leaflet() %>%
+      setView(-71.95883291,-36.53359195,14) %>%
+      addTiles('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',group='Satellite') %>%
+      addDrawToolbar(
+        targetGroup='draw',
+        editOptions = editToolbarOptions(selectedPathOptions = selectedPathOptions()))  %>%
+      addLayersControl(overlayGroups = c('draw'), options =
+                         layersControlOptions(collapsed=FALSE))
+  })
+  
+  output$mapaIndices <- renderLeaflet({
+    load(file='RData/raster_VIs_20161220.Rdata')
+    
+    op <- switch(input$VIindex,
+                 ndvi=1,
+                 ndwi=2,
+                 rendvi=3)
+    
+    names <- c('NDVI','NDWI','RE-NDVI')
+    img <- subset(vis_R,c(16:7,20))
+    
+    colors <- rev(colorRampPalette(c("darkgreen","yellow", "darkred"))(30))
+    colorsPal <- colorNumeric(colors, values(img[[op]]), na.color = "transparent")
+    
+    
+    leaflet() %>% 
+      # addTiles(group='Base'
+      #         ) %>%
+      #       addProviderTiles("OpenTopoMap", group = "DEM") %>%
+      addTiles('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',group='Satellite') %>%
+      addRasterImage(img[[op]], colors = colorsPal, opacity = 0.8,group=names[op]) %>%
+       addLegend(pal = colorsPal, values = values(img[[op]]),
+         title = names[op]) %>%
+      addLayersControl(
+        baseGroups = c("Satellite"),
+        overlayGroups = c(names[op]),
+        position="topright",
+        options = layersControlOptions(collapsed = FALSE)
+      )
+  })
+  
+  output$mapaSectores <- renderLeaflet({
+    load(file='RData/RF_classes.Rdata')
+    pal <- colorRampPalette(brewer.pal(11,"Spectral"))(5)
+    leaflet() %>% 
+      # addTiles(group='Base'
+      #         ) %>%
+      #       addProviderTiles("OpenTopoMap", group = "DEM") %>%
+      addTiles('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',group='Satellite') %>%
+      addRasterImage(rf_raster, colors=pal, opacity = 0.8) %>%
+      addLegend(colors=pal,labels=1:5) 
+  })
+  
+  output$plotHC <- renderHighchart({
+    load(file='RData/data.frame_sectores.Rdata')
+    dataG <- data %>% gather(indice,value,-sector) %>% mutate(value=round(value,1))
+    hchart(dataG, "column", hcaes(x = sector, y = value, group = indice))
+  })
+  
+  output$pieHC <- renderHighchart({
+    pal <- colorRampPalette(brewer.pal(11,"Spectral"))(5)
+    load(file='RData/RF_classes.Rdata')
+    df<-data.frame(freq(rf_raster)[1:5,1:2])
+    names(df) <- c('sector','value')
+    df$perc <- round(df$value*100/sum(df$value))
+    highchart() %>%
+      hc_chart(type = 'pie',
+               polar = TRUE) %>% 
+      hc_add_series(df$perc, name = "Área") %>% 
+      hc_add_series_labels_values(paste0('Sector ',df$sector),df$perc, name = "Área") %>% 
+      hc_plotOptions(
+        pie = list(
+          colors=pal,
+          colorByPoint = TRUE,
+          dataLabels = list(enabled = FALSE)
+        )) %>% 
+      hc_tooltip(pointFormat = "{point.y}%")
+    
+  })
+  # pkgStream is a reactive expression that represents a stream of
+  # new package download data; up to once a second it may return a
+  # data frame of new downloads since the last update.
+  output$rate <- renderValueBox({
+    valueBox(
+      value = 5,
+      subtitle = "Número de sectores",
+      icon = icon("area-chart"),
+      color = "aqua"
+    )
+  })
+  
+  output$count <- renderValueBox({
+    valueBox(
+      value = 319.6,
+      subtitle = "Superficie total (ha)",
+      icon = icon("download")
+    )
+  })
+  
+  output$users <- renderValueBox({
+    valueBox(
+      usrCount(),
+      "Unique users",
+      icon = icon("users")
+    )
+  })
+}
---
+++ ui.R
@@ -0,0 +1,70 @@
+library(highcharter)
+library(leaflet)
+
+dashboardPage(
+  dashboardHeader(title = "SmartFarm"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Seleccion", tabName = "seleccionMapa"),
+      menuItem("Estado vegetacional", tabName = "muestraIndices"),
+      menuItem("Clima", tabName = "climatica"),
+      menuItem("Dashboard", tabName = "dashboard")
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem("seleccionMapa",
+              box(
+                width = 8, status = "info", solidHeader = TRUE,
+                title = "Seleccione área de interes",
+              leafletOutput("mapSelect", width="100%", height=500))
+      ),
+      tabItem("muestraIndices",
+              box(
+                  width = 8, status = "info", solidHeader = TRUE,
+                  title = "Indice vegetacional área de interes",
+                  leafletOutput("mapaIndices", width="100%", height=500)
+                ),
+              box(
+                width = 2, status = "info", solidHeader = TRUE,
+                title = "Seleccionar",
+                selectInput("VIindex", "Índice:",
+                            c("NDVI" = "ndvi",
+                              "NDWI" = "ndwi",
+                              "RE-NDVI" = "rendvi"))
+              )
+      ),
+      tabItem("climatica",
+              tags$iframe(
+                seamless = "seamless", 
+                src = "https://forecast.io/embed/#lat=-36.3583&lon=-71.0603&name=Downtown Chillan", 
+                height = 300, width = 800)
+              ),
+      tabItem("dashboard",
+              fluidRow(
+                valueBoxOutput("rate"),
+                valueBoxOutput("count")
+               ),
+              fluidRow(
+                box(
+                  width = 4, status = "info", solidHeader = TRUE,
+                  title = "Sectores identificado",
+                  leafletOutput("mapaSectores", width="100%", height=600)
+                ),
+                fluidRow(
+                  box(
+                    width = 7, status = "info",solidHeader = TRUE,
+                    title = "Valores indices por sector",
+                    highchartOutput("plotHC",height = "300px")
+                ),
+                fluidRow(
+                  box(
+                    width = 3, status = "info",solidHeader = TRUE,
+                    title = "Superficie por sector",
+                    highchartOutput("pieHC",height='200px')
+                )))
+              )
+      )
+    )
+  )
+)
